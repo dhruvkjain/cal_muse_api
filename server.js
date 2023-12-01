3793489e7df465b245a5ed571442154f7819b0fa
Dhruv-K-Jain <138356622+Dhruv-K-Jain@users.noreply.github.com>
2023-12-02 03:46:23 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+app.use(express.json({ extended: false }));
+const cors = require('cors');
+app.use(cors());
+
+const { spawn } = require('child_process');
+
+app.post('/plot',(req,res)=>{
+  const pythonProcess = spawn('python', ['test.py', req.body.x, req.body.y, req.body.z, req.body.k, '.\jpg']);
+
+  pythonProcess.stdout.on('data', (data) => {
+    // console.log(`Python stdout: ${data}`); 
+    res.send(data);
+  });
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`Python stderr: ${data}`);
+    res.status(400).json("error");
+  });
+  // pythonProcess.on('close', (code) => {
+  //   console.log(`Python process exited with code ${code}`);
+  // });
+})
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT);
