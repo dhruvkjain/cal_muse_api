2ca26af77a14481ffb79ce336407530588e69f46
Dhruv-K-Jain <138356622+Dhruv-K-Jain@users.noreply.github.com>
2023-12-02 15:05:49 +0530

    data received

--- server.js
+++ server.js
@@ -6,39 +6,39 @@ app.use(cors());
 
 const { spawn } = require('child_process');
 
-const installProcess = spawn('pip', ['install', 'numpy']);
-installProcess.on('close', (code) => {
-  if (code === 0) {
-    console.log(`Package numpy installed successfully`);
-  } else {
-    console.error(`Error installing numpy`);
-  }
-});
+// const installProcess = spawn('pip', ['install', 'numpy']);
+// installProcess.on('close', (code) => {
+//   if (code === 0) {
+//     console.log(`Package numpy installed successfully`);
+//   } else {
+//     console.error(`Error installing numpy`);
+//   }
+// });
 
-// Capture and log the output of the process
-installProcess.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
+// // Capture and log the output of the process
+// installProcess.stdout.on('data', (data) => {
+//   console.log(`stdout: ${data}`);
+// });
 
-installProcess.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-});
+// installProcess.stderr.on('data', (data) => {
+//   console.error(`stderr: ${data}`);
+// });
 
 app.post('/plot',(req,res)=>{
   
-  // const pythonProcess = spawn('python', ['test.py', req.body.x, req.body.y, req.body.z, req.body.k, '.\jpg']);
+  const pythonProcess = spawn('python', ['test.py', req.body.x, req.body.y, req.body.z, req.body.k, '.\jpg']);
 
-  // pythonProcess.stdout.on('data', (data) => {
-  //   // console.log(`Python stdout: ${data}`); 
-  //   res.send(data);
-  // });
-  // pythonProcess.stderr.on('data', (data) => {
-  //   console.error(`Python stderr: ${data}`);
-  //   res.status(400).json(data);
-  // });
-  // pythonProcess.on('close', (code) => {
-  //   console.log(`Python process exited with code ${code}`);
-  // });
+  pythonProcess.stdout.on('data', (data) => {
+    // console.log(`Python stdout: ${data}`); 
+    res.send(data);
+  });
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`Python stderr: ${data}`);
+    res.status(400).json(data);
+  });
+  pythonProcess.on('close', (code) => {
+    console.log(`Python process exited with code ${code}`);
+  });
 })
 
 
