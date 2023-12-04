ce53cfb035e3f0d45a8415defd1a69e74565a2c2
Dhruv-K-Jain <138356622+Dhruv-K-Jain@users.noreply.github.com>
2023-12-04 19:08:26 +0530

    mpl_tookit removed

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+# from mpl_toolkits.mplot3d import Axes3D
 import struct
 import sys
 
